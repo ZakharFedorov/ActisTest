5f7ef0ea266d2e34a9c1bb5454e2d05e7d3e1cba
ZakharFedorov <zahar.fedo@mail.ru>
2023-10-05 18:40:36 +0200

    first commit

---
+++ src/test/java/com/example/actistest/ActisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actistest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActisTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
